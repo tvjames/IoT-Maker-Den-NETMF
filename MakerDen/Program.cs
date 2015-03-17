a46879f94bee9f6c5e96e5a86c5f04ada27b8600
Thomas James <thomasvjames@gmail.com>
2015-03-17 21:28:49 +1000

    Lab 3

--- MakerDen/Program.cs
+++ MakerDen/Program.cs
@@ -12,7 +12,29 @@ namespace MakerDen {
 
         public static void Main() {
             // main code marker
+            using (SensorTemp temp = new SensorTemp(Pins.GPIO_PIN_D8, 10000, "temp01"))
+            using (SensorLight light = new SensorLight(AnalogChannels.ANALOG_PIN_A0, 1000, "light01"))
+            using (var rgb = new RgbLed(Pins.GPIO_PIN_D3, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D6, "rgb01")) 
+            {
+                uint index = 0;
+                while (true) {
 
+                    Debug.Print(light.ToString());
+                    Debug.Print(temp.ToString());
+
+                    var led = RgbLed.Led.Red;
+                    if (index % 3 == 1)
+                        led = RgbLed.Led.Blue;
+                    if (index % 3 == 2)
+                        led = RgbLed.Led.Green;
+
+                    rgb.On(led);
+                    Thread.Sleep(500);
+                    rgb.Off(led);
+                    Thread.Sleep(500);
+                    index++;
+                }
+            }
         }
     }
 }
