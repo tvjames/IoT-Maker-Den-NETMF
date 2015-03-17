1f733c13e0aec9ecd02a7689c1a8ba164efb0b30
Thomas James <thomasvjames@gmail.com>
2015-03-17 22:21:45 +1000

    Lab 5

--- MakerDen/Program.cs
+++ MakerDen/Program.cs
@@ -6,34 +6,39 @@ using Glovebox.Netduino.Sensors;
 using Microsoft.SPOT;
 using SecretLabs.NETMF.Hardware.NetduinoPlus;
 using System.Threading;
+using Microsoft.SPOT.Net.NetworkInformation;
 
 namespace MakerDen {
     public class Program : MakerBaseIoT {
 
         public static void Main() {
             // main code marker
+            var interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            if (interfaces.Length > 0)
+            {
+                var tries = 0;
+                var ethernet = interfaces[0];
+                ethernet.EnableDhcp();
+                while (ethernet.IPAddress == "0.0.0.0" && tries < 120)
+                {
+                    Thread.Sleep(500);
+                    tries++;
+                }
+                Debug.Print("IP Address: " + ethernet.IPAddress);
+            }
+            
+            StartNetworkServices("tvj", true, "5C-86-4A-00-58-3C");
+
             using (SensorTemp temp = new SensorTemp(Pins.GPIO_PIN_D8, 10000, "temp01"))
             using (SensorLight light = new SensorLight(AnalogChannels.ANALOG_PIN_A0, 1000, "light01"))
-            using (var rgb = new RgbLed(Pins.GPIO_PIN_D3, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D6, "rgb01")) 
+            using ( rgb = new RgbLed(Pins.GPIO_PIN_D3, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D6, "rgb01")) 
             {
-                uint index = 0;
-                while (true) {
+                temp.OnBeforeMeasurement += OnBeforeMeasure;
+                temp.OnAfterMeasurement += OnMeasureCompleted;
+                light.OnBeforeMeasurement += OnBeforeMeasure;
+                light.OnAfterMeasurement += OnMeasureCompleted;
 
-                    Debug.Print(light.ToString());
-                    Debug.Print(temp.ToString());
-
-                    var led = RgbLed.Led.Red;
-                    if (index % 3 == 1)
-                        led = RgbLed.Led.Blue;
-                    if (index % 3 == 2)
-                        led = RgbLed.Led.Green;
-
-                    rgb.On(led);
-                    Thread.Sleep(500);
-                    rgb.Off(led);
-                    Thread.Sleep(500);
-                    index++;
-                }
+                Thread.Sleep(Timeout.Infinite);
             }
         }
     }
